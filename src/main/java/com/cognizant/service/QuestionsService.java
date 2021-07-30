8e7e416b9ca7a93f87a146c741faa32845a8f932
atulanand1811 <61856564+atulanand1811@users.noreply.github.com>
2021-07-30 14:13:55 +0530

    Add files via upload
    checklist

---
+++ src/main/java/com/cognizant/service/QuestionsService.java
@@ -0,0 +1,37 @@
+package com.cognizant.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.model.QuestionsEntity;
+import com.cognizant.repository.QuestionsRepository;
+/**
+ * @version 1.8
+ * 			This class is used to load the questions from database and
+ *          checking whether the question resides in database or not based on the
+ *          given id.
+ *
+ */
+@Service
+public class QuestionsService {
+	/**
+	 * 
+	 */
+	@Autowired
+	QuestionsRepository questionsRepository;
+	/**
+	 * 
+	 * @param auditType
+	 * @return List
+	 * @throws IndexOutOfBoundsException
+	 */
+	public List<QuestionsEntity> getQuestions(String auditType) throws IndexOutOfBoundsException {
+
+		if(questionsRepository.findByAuditType(auditType).isEmpty()) {
+			throw new IndexOutOfBoundsException();
+		}
+		return questionsRepository.findByAuditType(auditType);
+	}	
+}
