bf29dc5e688deb52d03dcb7391aa2eb3b182d897
atulanand1811 <61856564+atulanand1811@users.noreply.github.com>
2021-08-01 16:39:58 +0530

    Update AuditCheckListController.java
    cross origin added

--- src/main/java/com/cognizant/Controller/AuditCheckListController.java
+++ src/main/java/com/cognizant/Controller/AuditCheckListController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
